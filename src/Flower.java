0895facceaab78136d9cb7ec3a82837154855503
RavenVP <Raven_vp_72@mail.ru>
2022-10-03 23:04:33 +0500

    Выполнил HW 2-3-1-2

---
+++ src/Flower.java
@@ -0,0 +1,69 @@
+public class Flower {
+    private String flowerName;
+    private String country;
+    private double cost;
+    private int lifeSpan;
+
+    Flower(String flowerName,String country,double cost,int lifeSpan) {
+        if (flowerName == null || flowerName.isBlank() || flowerName.isEmpty()) {
+            this.flowerName = " ??Белый?? ";
+        } else {
+            this.flowerName = flowerName;
+        }
+        if (country == null || country.isBlank() || country.isEmpty()) {
+            this.country = " Россия ";
+        } else {
+            this.country = country;
+        }
+        if (cost <= 0 ) {
+            this.cost = 1;
+        } else {
+            this.cost = cost;
+        }
+        if (lifeSpan <= 0 ) {
+            this.lifeSpan = 3;
+        } else {
+            this.lifeSpan = lifeSpan;
+        }
+    }
+
+    public String getFlowerName() {
+        return flowerName;
+    }
+
+    public void setFlowerName(String flowerColor) {
+        this.flowerName = flowerColor;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getLifeSpan() {
+        return lifeSpan;
+    }
+
+    public void setLifeSpan(int lifeSpan) {
+        this.lifeSpan = lifeSpan;
+    }
+
+    void flowerInfo() {
+        System.out.println("");
+        System.out.println("Название --> " + flowerName );
+        System.out.println("Страна --> " + country );
+        System.out.println("Цена за штуку --> " + cost );
+        System.out.println("Время жизни --> " + lifeSpan );
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Flower rose = new Flower("Роза обычкновенная ","Голландия", 35.59, 0);
+        Flower chrys = new Flower("Хризантема",null, 15, 5);
+        Flower peony = new Flower("Пион","Англия", 69.9, 1);
+        Flower gyps = new Flower("Гипсофила","Турция", 19.5, 10);
+
+        rose.flowerInfo();
+        chrys.flowerInfo();
+        peony.flowerInfo();
+        gyps.flowerInfo();
+
     }
 }
